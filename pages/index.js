1646ad21d176bf1c96d345d5fccd4084956aa9cd
akyerzeki-code <akyerzeki@gmail.com>
2025-08-14 23:26:18 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+      <h1>TAHMİN360</h1>
+      <p>Site başarıyla yayınlandı! (Pages Router)</p>
+    </main>
+  );
+}

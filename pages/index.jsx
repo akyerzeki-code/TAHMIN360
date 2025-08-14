54470607cd99a2729326dc662ed818c8037424b7
akyerzeki-code <akyerzeki@gmail.com>
2025-08-15 01:35:19 +0300

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return React.createElement(
+    "div",
+    { style: { padding: "20px", fontFamily: "Arial, sans-serif" } },
+    React.createElement("h1", null, "TAHMIN360"),
+    React.createElement("p", null, "Site basariyla calisiyor!")
+  );
+}

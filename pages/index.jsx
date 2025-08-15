f2026a5ca0a183cf3298c2f415140b88c020e4e5
akyerzeki-code <akyerzeki@gmail.com>
2025-08-15 06:59:23 +0300

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1>TAHMIN360</h1>
+      <p>Site başarıyla çalışıyor!</p>
+    </div>
+  );
+}
